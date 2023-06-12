946a29ac0d19f2087f89404ef605eef2de36a705
Agnès Haasser <agnes.haasser@beta.gouv.fr>
2023-06-12 14:22:53 +0200

    Add account urls and login template

--- secretariat/urls.py
+++ secretariat/urls.py
@@ -1,8 +1,19 @@
-from django.urls import path
+from django.urls import include, path
 
 from secretariat import views
 
 urlpatterns = [
     path("", views.view_index, name="index"),
     path("accessibilite/", views.view_accessibilite, name="accessibilite"),
+    path("compte/", include("django.contrib.auth.urls")),
 ]
+
+# see https://docs.djangoproject.com/en/4.2/topics/auth/default/#using-the-views
+# compte/login/ [name='login']
+# compte/logout/ [name='logout']
+# compte/password_change/ [name='password_change']
+# compte/password_change/done/ [name='password_change_done']
+# compte/password_reset/ [name='password_reset']
+# compte/password_reset/done/ [name='password_reset_done']
+# compte/reset/<uidb64>/<token>/ [name='password_reset_confirm']
+# compte/reset/done/ [name='password_reset_complete']
