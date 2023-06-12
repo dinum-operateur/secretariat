009b75b4663b685de4dd33d305ff4ed59f2fc698
Agnès Haasser <agnes.haasser@beta.gouv.fr>
2023-06-12 14:39:27 +0200

    Remove app "widget_tweaks"

--- config/settings.py
+++ config/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "widget_tweaks",
     "dsfr_holder",
     "secretariat",
 ]
