73fc6fc9e8ed077ad4b04865432496ea85e2cfc2
Marie J <marie.jeammet@beta.gouv.fr>
2023-10-05 16:57:15 +0200

    new users are invited to outline

--- secretariat/models.py
+++ secretariat/models.py
@@ -1,5 +1,32 @@
+import requests
 from django.contrib.auth.models import AbstractUser
+
+from config.settings import OUTLINE_API_TOKEN, OUTLINE_API_URL
 
 
 class User(AbstractUser):
     pass
+
+    def save(self, *args, **kwargs):
+        super(User, self).save(*args, **kwargs)
+        self.invite_to_outline()
+
+    def invite_to_outline(self):
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": f"Bearer {OUTLINE_API_TOKEN}",
+        }
+        requests.post(
+            url=f"{OUTLINE_API_URL}/users.invite",
+            headers=headers,
+            json={
+                "invites": [
+                    {
+                        "name": f"{self.first_name} {self.last_name}",
+                        "email": self.email,
+                        "role": "member",
+                    }
+                ]
+            },
+        )
--- secretariat/tests.py
+++ secretariat/tests.py
@@ -12,8 +12,7 @@ class TestUser(TestCase):
         "Authorization": f"Bearer {OUTLINE_API_TOKEN}",
     }
 
-    def test_creating_user_here_adds_them_to_outline(self):
-        print(OUTLINE_API_URL)
+    def list_outline_users(self, query):
         response = requests.post(
             url=f"{OUTLINE_API_URL}/users.list",
             headers=self.headers,
@@ -22,29 +21,27 @@ class TestUser(TestCase):
                 "limit": 25,
                 "sort": "updatedAt",
                 "direction": "DESC",
-                "query": "pcrandall",
+                "query": query,
                 "filter": "all",
             },
         )
-        print(response)
+        return response
+
+    def test_creating_user_here_adds_them_to_outline(self):
+        user = User(
+            first_name="prudence",
+            last_name="crandall",
+            username="pcrandall",
+            email="prudence.crandall@gouv.fr",
+            password="whatever",
+        )
+
+        response = self.list_outline_users(query=f"{user.first_name} {user.last_name}")
         self.assertEqual(response.json()["pagination"]["total"], 0)
 
-        user = User(
-            username="pcrandall", email="prudence.crandall@gouv.fr", password="whatever"
-        )
         user.save()
         self.assertTrue(user in User.objects.all())
 
-        response = requests.post(
-            url=f"{OUTLINE_API_URL}/users.list",
-            headers=self.headers,
-            json={
-                "offset": 0,
-                "limit": 25,
-                "sort": "updatedAt",
-                "direction": "DESC",
-                "query": "pcrandall",
-                "filter": "all",
-            },
-        )
-        self.assertTrue(response.json()["pagination"]["total"], 1)
+        response = self.list_outline_users(query=f"{user.first_name} {user.last_name}")
+        self.assertEqual(response.json()["pagination"]["total"], 1)
+        self.assertEqual(response.json()["data"][0]["email"], user.email)
