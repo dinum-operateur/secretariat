74fd3c7cd42e8512a00a013030347e74413e66fe
Marie J <marie.jeammet@beta.gouv.fr>
2023-10-16 18:56:31 +0200

    raise exception and message when invitation fails

--- secretariat/admin.py
+++ secretariat/admin.py
@@ -1,10 +1,12 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 
+from config.settings import OUTLINE_OPI_GROUP_ID
 from secretariat.models import User
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
+
     list_display = (
         "username",
         "email",
@@ -14,3 +16,22 @@ class UserAdmin(admin.ModelAdmin):
         "is_staff",
     )
     readonly_fields = ["outline_uuid"]
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+
+        if obj.outline_uuid is None:
+            from secretariat.utils.outline import Client as OutlineClient
+            from secretariat.utils.outline import InvitationFailed
+
+            client = OutlineClient()
+
+            try:
+                obj.outline_uuid = client.invite_to_outline(obj)
+                obj.save()
+                client.add_to_outline_group(
+                    user_uuid=obj.outline_uuid, group=OUTLINE_OPI_GROUP_ID
+                )
+            except InvitationFailed:
+                error_message = f"L'invitation a Outline a échoué. Vérifiez que l'email \"{obj.email}\" n'est pas déjà invitée sur Outline."
+                messages.warning(request, error_message)
--- secretariat/models.py
+++ secretariat/models.py
@@ -1,22 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from config.settings import OUTLINE_OPI_GROUP_ID
-
 
 class User(AbstractUser):
     outline_uuid = models.CharField(max_length=36, blank=False, null=True, default=None)
-
-    def save(self, *args, **kwargs):
-        super(User, self).save(*args, **kwargs)
-
-        if self.outline_uuid is None:
-            from secretariat.utils.outline import Client as OutlineClient
-
-            client = OutlineClient()
-
-            self.outline_uuid = client.invite_to_outline(self)
-            self.save()
-            client.add_to_outline_group(
-                user_uuid=self.outline_uuid, group=OUTLINE_OPI_GROUP_ID
-            )
