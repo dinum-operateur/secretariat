735290479b2a7960667eea22a0489d2feb79fc66
Marie J <marie.jeammet@beta.gouv.fr>
2023-10-17 16:23:14 +0200

    invite when no uuid

--- secretariat/admin.py
+++ secretariat/admin.py
@@ -19,7 +19,7 @@ class UserAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         super().save_model(request, obj, form, change)
 
-        if "email" in form.changed_data:
+        if obj.outline_uuid is None and "email" in form.changed_data:
             from secretariat.utils.outline import Client as OutlineClient
             from secretariat.utils.outline import InvitationFailed
 
@@ -31,6 +31,8 @@ class UserAdmin(admin.ModelAdmin):
                 client.add_to_outline_group(
                     user_uuid=obj.outline_uuid, group=OUTLINE_OPI_GROUP_ID
                 )
+                success_message = f"Mail « {obj.email} » invitée sur Outline et ajoutée au groupe Opérateur."
+                messages.success(request, success_message)
             except InvitationFailed:
                 error_message = f"L’invitation à Outline a échoué. Vérifiez que l'adresse email « {obj.email} » n'est pas déjà invitée sur Outline."
                 messages.warning(request, error_message)
