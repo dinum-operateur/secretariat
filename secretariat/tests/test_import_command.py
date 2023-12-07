53218049abf6f33e08aefb5fa8088b9a4e12e402
Marie PUPO JEAMMET <marie.jeammet@beta.gouv.fr>
2023-12-07 16:44:25 +0100

    test membership import

--- secretariat/tests/test_import_command.py
+++ secretariat/tests/test_import_command.py
@@ -1,8 +1,12 @@
 from django.core.management import call_command
 from django.test import TestCase
 
-from secretariat.models import Organisation, User
-from secretariat.tests.factories import OrganisationFactory, UserFactory
+from secretariat.models import Membership, Organisation, User
+from secretariat.tests.factories import (
+    MembershipFactory,
+    OrganisationFactory,
+    UserFactory,
+)
 from secretariat.utils.outline import Client as OutlineClient
 
 
@@ -21,12 +25,12 @@ class TestSynchronizationCommand(TestCase):
 
         self.assertTrue(
             User.objects.filter(email=outline_user.email).exists(),
-            "User should have been imported.",
+            "User should now exist.",
         )
 
         self.outline_client.remove_user_from_outline(outline_user)
 
-    def test_importing_from_outline_imports_new_orga(self):
+    def test_importing_from_outline_imports_new_empty_orga(self):
         outline_group = OrganisationFactory.build()
         outline_group.outline_group_uuid = self.outline_client.create_new_group(
             outline_group.name
@@ -40,7 +44,35 @@ class TestSynchronizationCommand(TestCase):
 
         self.assertTrue(
             Organisation.objects.filter(name=outline_group.name).exists(),
-            "Organization should exist now.",
+            "Organization should now exist.",
         )
 
         self.outline_client.delete_group_from_outline(outline_group)
+
+    def test_importing_from_outline_imports_new_orga_and_memberships(self):
+        outline_membership = MembershipFactory.build()
+        self.outline_client.add_to_outline_group(
+            outline_membership.user, outline_membership.organisation
+        )
+
+        self.assertFalse(
+            Membership.objects.filter(
+                user=outline_membership.user,
+                organisation=outline_membership.organisation,
+            ).exists(),
+            "Membership should not exist on Django (yet)",
+        )
+
+        call_command("import-from-outline")
+        self.assertTrue(
+            Membership.objects.filter(
+                user=outline_membership.user,
+                organisation=outline_membership.organisation,
+            ).exists(),
+            "Membership should now exist",
+        )
+
+        self.outline_client.remove_user_from_outline(
+            outline_membership.user,
+        )
+        self.outline_client.delete_group_from_outline(outline_membership.organisation)
