f743c0dc53d992bf49dbff36db25c5b33fec6ccb
Marie PUPO JEAMMET <marie.jeammet@beta.gouv.fr>
2023-12-15 18:00:29 +0100

    login with admin

--- secretariat/views.py
+++ secretariat/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.shortcuts import redirect, render
 
 
 def view_index(request):
@@ -7,3 +8,8 @@ def view_index(request):
 
 def view_accessibilite(request):
     return render(request, "secretariat/accessibilite.html")
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("index")
